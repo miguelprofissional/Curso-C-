60cef868bc73c73435a6fb65c07e9164745f1575
miguelprofissional <miguelprofissional8@gmail.com>
2022-03-28 12:22:31 -0300

    Aula 04

---
+++ Aula 04/Aula 04.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+
+
+int vidas=0;
+char letra ='0';
+double decimal = 5.2222;
+float decimal2 = 5.2;
+bool vivo = true;
+string nome = "Diego";
+
+cout <<"Digite o numero de vidas:";
+cin >> vidas;
+cout << "Digite uma letra:";
+cin >> letra;
+cout << "Dinheiro";
+cin >> decimal;
+cout << "Digite seu Nome:";
+cin >> nome;
+
+
+
+cout << vidas << "\n" <<letra << "\n" << decimal << "\n" << decimal2 << vivo << "\n" << nome << "\n";
+
+return 0;
+
+}
